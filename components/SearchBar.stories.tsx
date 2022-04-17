1ad1a83103933e7143196aa80f1b08360ee11b03
Stygmates <styg.tan.dat@gmail.com>
2022-04-17 07:35:17 +0000

    fix(Storybook): :bug: Add missing store

--- components/SearchBar.stories.tsx
+++ components/SearchBar.stories.tsx
@@ -1,4 +1,6 @@
 import { ComponentMeta } from "@storybook/react";
+import { Provider } from "react-redux";
+import { store } from "../app/store";
 import SearchBar from "./SearchBar";
 
 export default {
@@ -7,5 +9,5 @@ export default {
 } as ComponentMeta<typeof SearchBar>;
 
 export const Primary = () => {
-  return <SearchBar />;
+  return <Provider store={store}><SearchBar /></Provider>;
 };
