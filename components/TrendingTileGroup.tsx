f20653bdead4d395ff945e3618c9fefa048332bf
Stygmates <styg.tan.dat@gmail.com>
2022-04-17 13:09:34 +0200

    fix: :sparkles: Hide trending tiles when unnecessary

--- components/TrendingTileGroup.tsx
+++ components/TrendingTileGroup.tsx
@@ -4,16 +4,37 @@ import { Tile, TileGroup } from "./Tile";
 import TrendingTile from "./TrendingTile";
 import { useSelector } from "react-redux";
 import { selectTiles } from "../features/tiles/TilesSlice";
+import { selectSearchBar } from "../features/searchBar/SearchBarSlice";
+import { selectActiveTab } from "../features/activeTab/ActiveTabSlice";
 type scrollVisibilityApiType = React.ContextType<typeof VisibilityContext>;
 
 type Props = {};
 export default function TrendingTileGroup({}: Props) {
   const tiles = useSelector(selectTiles);
-  return <PureTrendingTileGroup tiles={tiles} />;
+  const searchBarValue = useSelector(selectSearchBar);
+  const activeTab = useSelector(selectActiveTab);
+  return (
+    <PureTrendingTileGroup
+      tiles={tiles}
+      searchBarValue={searchBarValue}
+      activeTab={activeTab}
+    />
+  );
 }
 
-export function PureTrendingTileGroup({ tiles }: { tiles: TileGroup | null }) {
+export function PureTrendingTileGroup({
+  tiles,
+  searchBarValue,
+  activeTab,
+}: {
+  tiles: TileGroup | null;
+  searchBarValue: string | null;
+  activeTab: string | null;
+}) {
   const { disableScroll, enableScroll } = usePreventBodyScroll();
+  if (searchBarValue || activeTab == "Bookmarked") {
+    return <></>;
+  }
   if (tiles == null) {
     return <div className="heading-l">Nothing to display</div>;
   }
